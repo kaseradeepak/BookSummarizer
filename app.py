77be5436a12165134a420657fac542c8900a0fc9
kaseradeepak <deepak.kasera@scaler.com>
2026-03-26 21:44:00 +0530

    Book Summarizer Project

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from openai import OpenAI
-from utils import read_text_from_file
+from utils import read_text_from_file, chunk_text
 
 client = OpenAI()
 
@@ -7,9 +7,23 @@ client = OpenAI()
 
 book_text = read_text_from_file('/Users/deepakkasera/book-summarizer/book.txt')
 
-response = client.responses.create(
-    model = "gpt-5.4",
-    input = f"Summarize this book in simple words to a 10 year old kid: {book_text}"
-)
+# Create chunks from the book_text.
+chunks = chunk_text(book_text, 50)
 
-print(response.output_text)
+print(f"Number of chunks: {len(chunks)}")
+
+for chunk in chunks:
+    chunk_summary = client.responses.create(
+        model = "gpt-5.4",
+        input = f"Summarize this text: {chunk}"
+    )
+
+    print(chunk_summary.output_text, end="==============")
+
+
+# response = client.responses.create(
+#     model = "gpt-5.4",
+#     input = f"Summarize this book in simple words to a 10 year old kid: {book_text}"
+# )
+
+# print(response.output_text)
--- utils.py
+++ utils.py
@@ -1,6 +1,31 @@
 from pathlib import Path
 
+# Chunking logic
+# chunk_size = 1000
+def chunk_text(text: str, chunk_size: int = 1000) -> list[str]:
+    words = text.split(' ') # list of words from the text.
+    chunks = []
+    
+    i = 0
+    while i < len(words):
+        chunk = words[i : i + chunk_size - 1]
+        chunks.append(chunk)
+        i = i + chunk_size
+    
+    return chunks
+
+
 def read_text_from_file(file_path: str) -> str:
     path = Path(file_path)
     with path.open("r", encoding="utf-8") as file:
         return file.read()
+
+
+# 18 words
+# chunk_size = 5
+# chunks = ["Give models access to new", "functionality and data they can", "", ""]
+
+# i = 5
+# Give models access to new functionality and data they can use to follow instructions and respond to prompts.
+
+# chunk = "Give models access to new"
